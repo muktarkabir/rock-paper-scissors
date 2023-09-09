09abb18d2d679977670284bc4a1a615575032e9e
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-09-09 19:30:01 +0100

    Create a function that plays one round of the game

--- script.js
+++ script.js
@@ -7,4 +7,29 @@ if (randomChoice == 1) {
 }else {
     return 'Scissors'}
 }
-  
+function playRound(playerSelection,computerSelection){
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = getComputerChoice();
+    if (playerSelection == computerSelection) {
+        console.log('Tie Game');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        console.log('You lose,Paper beats Rock.');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        console.log('You win,Rock beats Scissors.');
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log('You win,Paper beats Rock.');
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        console.log('You lose,Scissors beats Paper.');
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        console.log('You win, Scissors beats Paper');
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        console.log('CYou lose,Rock beats Scissors.');
+    } else{console.log('Invalid Selection');}
+
+    return playerSelection,computerSelection;
+    
+}  
+
+playerSelection = "rock";
+computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
