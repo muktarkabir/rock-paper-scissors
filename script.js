9acf294206a1bcbf50fe02387c0917eea803ba04
Muktar Kabir <mukhtarkabir3394@gmail.com>
2023-09-19 18:18:48 +0100

    Add event listeners with correct playerSelection

--- script.js
+++ script.js
@@ -8,16 +8,11 @@ if (randomChoice == 1) {
     return 'Scissors'}
 }
 
-
-let playResult = '';
 function playRound(playerSelection,computerSelection){
-    let alarm = 'Wrong Input.';
-    playerSelection = prompt('Enter Rock,Paper or Scissors:');
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    computerSelection = getComputerChoice();
-    
+    let playResult = '';
+    computerSelection = getComputerChoice();   
     if (playerSelection == computerSelection) {
-        playResult = 'Tie Game.';
+        playResult = 'Draw.';
     } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
         playResult = 'You lose,Paper covers Rock.';
     } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
@@ -30,54 +25,62 @@ function playRound(playerSelection,computerSelection){
         playResult = 'You win, Scissors cuts Paper!';
     } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
         playResult = 'You lose,Rock breaks Scissors.'
-    } else{alert(alarm); return playRound()}//returns the function when an input is invalid instead of using a do while.
-    alert(playResult);
+    }
     console.log(playResult);
     return playResult;
 }  
 
 
+const rockButton = document.getElementById('rock');
+rockButton.addEventListener('click',()=>{playRound('Rock')});
+const paperButton = document.getElementById('paper');
+paperButton.addEventListener('click',()=>{playRound('Paper')});
+const scissorsButton = document.getElementById('scissors');
+scissorsButton.addEventListener('click',()=>{playRound('Scissors')});
 
-function game() {
-    let wins = 0;
-    let losses = 0;
-    let ties = 0;
-    let finalMessage = '';
+
+
+
+// function game() {
+//     let wins = 0;
+//     let losses = 0;
+//     let ties = 0;
+//     let finalMessage = '';
     
-    for (let roundCount = 0; roundCount < 5; roundCount++) {
-        playRound();
-    if (playResult == 'You win,Rock breaks Scissors!') {
-        wins +=1;
-    }else if (playResult == 'You win,Paper covers Rock!') {
-        wins +=1;
-    }else if (playResult == 'You win, Scissors cuts Paper!') {
-        wins +=1;
-    }else if (playResult == 'You lose,Paper covers Rock.') {
-        losses += 1;
-    }else if (playResult == 'You lose,Scissors cuts Paper.') {
-        losses +=1;
-    }else if (playResult == 'You lose,Rock breaks Scissors.') {
-        losses +=1;
-    }else if (playResult =='Tie Game.') {
-        ties += 1;
-    } 
+//     for (let roundCount = 0; roundCount < 5; roundCount++) {
+//         playRound();
+//     if (playResult == 'You win,Rock breaks Scissors!') {
+//         wins +=1;
+//     }else if (playResult == 'You win,Paper covers Rock!') {
+//         wins +=1;
+//     }else if (playResult == 'You win, Scissors cuts Paper!') {
+//         wins +=1;
+//     }else if (playResult == 'You lose,Paper covers Rock.') {
+//         losses += 1;
+//     }else if (playResult == 'You lose,Scissors cuts Paper.') {
+//         losses +=1;
+//     }else if (playResult == 'You lose,Rock breaks Scissors.') {
+//         losses +=1;
+//     }else if (playResult =='Draw.') {
+//         ties += 1;
+//     } 
     
     
-    }
-    if (wins > losses) {
-        finalMessage = `Congratulations! You defeated the machine ${wins} to ${losses}.` 
-    }else if (losses > wins){
-        finalMessage = `You lost to the Computer ${losses} to ${wins}.`
-    }else{
-        finalMessage = 'It\'s a Draw.';
-    }
-    alert(finalMessage);
-    console.log(finalMessage,wins,losses,ties);
+//     }
+//     if (wins > losses) {
+//         finalMessage = `Congratulations! You defeated the machine ${wins} to ${losses}.` 
+//     }else if (losses > wins){
+//         finalMessage = `You lost to the Computer ${losses} to ${wins}.`
+//     }else{
+//         finalMessage = 'It\'s a Draw.';
+//     }
+//     alert(finalMessage);
+//     console.log(finalMessage,wins,losses,ties);
     
 
-}
+// }
 
-// game()
+
 
 
 
